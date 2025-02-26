2b023396428fde2024dc9aabca046fc795c426a3
Dan <den6135484@gmail.com>
2025-02-26 15:29:07 +0200

    semantic changes, inline onclick listeners moved to outer events

--- scripts.js
+++ scripts.js
@@ -1,3 +1,9 @@
+document.querySelector("header div.signin").
+    addEventListener("click", () => switchLoginWindow());
+
+document.querySelector("header div.checkbox-wrapper").
+    addEventListener("click", () => emulateCheckboxClick())
+
 function switchLanguageSelector(){
     let selector = document.querySelector("span.language-selector-wrapper > div");
     if(selector) selector.classList.toggle('hidden');
@@ -6,4 +12,9 @@ function switchLanguageSelector(){
 function switchLoginWindow(){
     let form = document.querySelector("header .signin-menu");
     if(form) form.classList.toggle('hidden');
+}
+
+function emulateCheckboxClick(){
+    let checkbox = document.querySelector("header div.checkbox-wrapper > input");
+    if(checkbox) checkbox.checked = !checkbox.checked;
 }
